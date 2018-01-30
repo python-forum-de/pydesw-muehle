9335c42cbd234259b1c2e614d92847926f9bcd70
sebastian2443 <sebastian2443@yahoo.de>
2018-01-30 11:15:34 +0100

    Update logic.py __eq__ Methode für Field

--- logic.py
+++ logic.py
@@ -16,7 +16,10 @@ class Field(namedtuple('Field', 'square_name, index, owner')):
         if not MIN_INDEX <= index <= MAX_INDEX:
             raise ValueError('Field index out of range')
         return tuple.__new__(cls, (square_name, index, owner))
- 
+
+    def __eq__(self, other):
+        return self.square_name == other.square_name and self.index == other.index and self.owner == other.owner
+         
 class Board(object):
     def __init__(self):
         self.fields = {
