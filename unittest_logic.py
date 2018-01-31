8d7347a69c3fb06e097e2b467bddfa396637248a
Sebastian Linke <Seb_Linke@arcor.de>
2018-01-31 23:55:56 +0100

    Verbesserte Tests
    - Verwendung von geeigneten Methoden des Test-Frameworks
    - Vermeidung einiger komplexer Konstrukte
    - Aufruf von unittest.main() zum Starten

--- unittest_logic.py
+++ unittest_logic.py
@@ -7,47 +7,30 @@ class TestBoard(unittest.TestCase):
         board = logic.Board()
         walter = logic.Player('Walter', 'white')
 
-        board.move_piece(walter, new_field=logic.Field(square_name='outer', index=0, owner=walter))
-        board.move_piece(walter, new_field=logic.Field(square_name='outer', index=1, owner=walter))
-        board.move_piece(walter, new_field=logic.Field(square_name='outer', index=2, owner=walter))
-        
-        field = logic.Field('outer', 0, walter)
-        self.assertEqual(board.is_in_mill(field), True)
-        
-        field = logic.Field('outer', 1, walter)
-        self.assertEqual(board.is_in_mill(field), True)
+        for i in in range(3):
+            board.move_piece(walter, new_field=logic.Field('outer', i, walter))
 
-        field = logic.Field('outer', 2, walter)
-        self.assertEqual(board.is_in_mill(field), True)
-
+        for field in board.get_all_fields(walter)['outer']:
+            self.assertTrue(board.is_in_mill(field))
+        
         field = logic.Field('mid', 1, walter)
-        self.assertEqual(board.is_in_mill(field), False)
+        self.assertFalse(board.is_in_mill(field))
 
     def test_get_allowed_fields(self):
         board = logic.Board()
         walter = logic.Player('Walter', 'white')
         peter = logic.Player('Peter', 'black')
 
-        board.move_piece(walter, new_field=logic.Field(square_name='outer', index=0, owner=walter))
-        board.move_piece(walter, new_field=logic.Field(square_name='mid', index=1, owner=walter))
-        board.move_piece(walter, new_field=logic.Field(square_name='outer', index=2, owner=walter))
+        for square, i in [('outer', 0), ('mid', 1), ('outer', 2)]:
+            board.move_piece(walter, new_field=logic.Field(square, i, walter))
+        board.move_piece(peter, new_field=logic.Field('outer', 1, peter))
         
-        board.move_piece(peter, new_field=logic.Field(square_name='outer', index=1, owner=peter))
+        allowed = board.get_allowed_fields(logic.Field('mid', 1, walter))
         
-        field = logic.Field(square_name='mid', index=1, owner=walter)
-        result = board.get_allowed_fields(field)
-
-        field = logic.Field(square_name='mid', index=0, owner=None)
-        self.assertEqual(result['mid'][0].__eq__(field), True)
-
-        field = logic.Field(square_name='mid', index=2, owner=None)
-        self.assertEqual(result['mid'][1].__eq__(field), True)
-
-        field = logic.Field(square_name='inner', index=1, owner=peter)
-        self.assertEqual(result['inner'][0].__eq__(field), False)
+        self.assertIn(logic.Field('mid', 0, walter), allowed['mid'])
+        self.assertIn(logic.Field('mid', 2, walter), allowed['mid'])
+        self.assertNotIn(logic.Field('inner', 1, peter), allowed['inner'])
 
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().loadTestsFromTestCase(TestBoard)
-    unittest.TextTestRunner(verbosity=2).run(suite)
-    #unittest.main()
+    unittest.main(verbosity=2)
